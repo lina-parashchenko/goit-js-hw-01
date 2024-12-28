ca5e02255198f9426ae396e65650e51ad23bfe98
Lina Parashchenko <blond2109@gmail.com>
2024-12-28 13:25:43 +0200

    delete-js

--- JS/task-3.js
+++
@@ -1,4 +0,0 @@
-function getElementWidth(content, padding, border) {
-     return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
-}
-console.log(getElementWidth("50px", "8px", "4px"));
